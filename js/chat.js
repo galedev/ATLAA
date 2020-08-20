b68827ca072c425acbe364569b00629dfdffdb22
Luiz Eduardo <tutoriais.gamehelp@gmail.com>
2020-08-20 19:17:49 -0300

    update chat.js

--- js/chat.js
+++ js/chat.js
@@ -5,7 +5,7 @@ function conectar(){
 
     conn.onopen = function(e) {
         console.log('Conectado no chat!');
-        //$('#conteudo-chat-mesa').load(' #conteudo-chat-mesa');
+        $('#conteudo-chat-mesa').load(' #conteudo-chat-mesa');
     };
 
     conn.onclose = function(e) {
